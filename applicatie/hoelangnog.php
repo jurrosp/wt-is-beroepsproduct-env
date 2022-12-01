dc486d6dd470c5b50f6ca37fe15ab57f1d243f24
jurrosp <94122132+jurrosp@users.noreply.github.com>
2022-12-01 03:03:09 +0100

    made it look prettier

--- applicatie/hoelangnog.php
+++ applicatie/hoelangnog.php
@@ -1,9 +1,9 @@
 <?php
 header('Refresh:0');
+
 $naam = 'Jurre Peeters';
 
 $vandaag = date_create('now');
-//$datum = $vandaag->format('d F Y');
 $event = date_create('2022/12/25');
 $wachtTijd = date_diff($vandaag, $event);
 $timeBetween = $wachtTijd->format('%m maanden %d dagen %H uur %i minuten %s seconden');
@@ -13,10 +13,10 @@ $timeBetween = $wachtTijd->format('%m maanden %d dagen %H uur %i minuten %s seco
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <title>PHP voorbeeld</title>
+    <title>Hoelang moeten we nog wachten?</title>
 </head>
 <body>
-    Welkom <?= $naam ?>!<br>
-    Het duurt nog <?= $timeBetween?> tot kerst.
+    <strong>Welkom <?= $naam ?>!</strong><br><br>
+    Het duurt nog <i><?= $timeBetween?></i> tot <strong>kerst.</strong>
 </body>
 </html>
