59e299eaee16e19facace52777ec59d3c34d97cd
jurrosp <94122132+jurrosp@users.noreply.github.com>
2022-12-01 23:10:31 +0100

    added other holidays via get

--- applicatie/hoelangnog.php
+++ applicatie/hoelangnog.php
@@ -3,10 +3,19 @@ header('Refresh:0');
 
 $naam = 'Jurre Peeters';
 
+//var_dump($_GET['omschrijving']);
+//var_dump($_GET['datum']);
+
+$holiday = $_GET['omschrijving'];
+$datum = $_GET['datum'];
+
 $vandaag = date_create('now');
-$event = date_create('2022/12/25');
+$event = date_create($datum);
 $wachtTijd = date_diff($vandaag, $event);
 $timeBetween = $wachtTijd->format('%m maanden %d dagen %H uur %i minuten %s seconden');
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -17,6 +26,7 @@ $timeBetween = $wachtTijd->format('%m maanden %d dagen %H uur %i minuten %s seco
 </head>
 <body>
     <strong>Welkom <?= $naam ?>!</strong><br><br>
-    Het duurt nog <i><?= $timeBetween?></i> tot <strong>kerst.</strong>
+    Het duurt nog <i><?= $timeBetween?></i> tot <strong><?= $holiday?>.</strong>
+    
 </body>
 </html>
