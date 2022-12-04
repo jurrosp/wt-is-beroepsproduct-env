5a1deff377ce700e0d9f0afea15b42b14d29cebb
jurrosp <94122132+jurrosp@users.noreply.github.com>
2022-12-04 23:37:38 +0100

    update arrays.php

--- applicatie/arrays.php
+++ applicatie/arrays.php
@@ -2,7 +2,16 @@
 $eenTMTien = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $zesTMVijftien = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 $samengevoegd = array_unique(array_merge($eenTMTien, $zesTMVijftien));
-print_r($samengevoegd);
+//print_r($samengevoegd);
+
+$film = [
+    'titel' => 'The Lord of The Rings',
+    'jaar' => 2012,
+    'regisseur' => 'Peter Jackson',
+    'hoofdrolspelers' => 'Ian McKellen, Martin Freeman, Richard Armitage, James Nesbitt, Ken Stott, Ian Holm, Christopher Lee, Hugo Weaving, Elijah Wood, Andy Serkins'
+];
+
+print_r($film);
 ?>
 <!DOCTYPE html>
 <html lang="nl">
