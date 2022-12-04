60e3eef45fccab85ebefce7d2ca1d2487f8301ad
jurrosp <94122132+jurrosp@users.noreply.github.com>
2022-12-04 21:00:05 +0100

    Create arrays.php

---
+++ applicatie/arrays.php
@@ -0,0 +1,16 @@
+<?php
+$eenTMTien = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$zesTMVijftien = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+$samengevoegd = array_unique(array_merge($eenTMTien, $zesTMVijftien));
+print_r($samengevoegd);
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Hallo</title>
+</head>
+<body>
+    <h1><strong>Welkom!</strong></h1>
+</body>
+</html>
