503871115ca51ab7553980ea878d1079108e44e5
jurrosp <94122132+jurrosp@users.noreply.github.com>
2022-12-13 12:15:25 +0100

    edited bp

--- applicatie/menukaart.php
+++ applicatie/menukaart.php
@@ -1,10 +1,4 @@
 <?php
-
-$soort;
-if (isset($_GET['soort'])) {
-  $soort = $_GET['soort'];
-}
-
 $menu = [
   "Eten" => [
     "Shoarma" => 6.95,
@@ -18,67 +12,73 @@ $menu = [
     "Fernandes" => 2.50,
     "Bier" => 5.50
   ],
-  "Toetjes" => [
-    "Ijsje" => 4.39,
-    "Kaasplank" => 7.42
+  'Toetjes' => [
+    'IJsje' => 4.50, 
+    'Kaasplank' => 7.41
   ]
 ];
-function genereerMenukaart($menu)
-{
-  $html = "";
-  foreach ($menu as $categorie => $producten) {
-    $html .= "<h2>$categorie</h2>";
-    $html .= genereerProducten($producten);
-  }
-  return $html;
-}
-function genereerProducten($producten)
-{
-  $html = "<table>";
-  if (isset($_GET['soort'])) {
-    $titel = $_GET['soort'];
-    $html .= "<h2>$titel</h2>";
-  }
-  foreach ($producten as $product => $prijs) {
-    $html .= "<tr>";
-    $html .= "<td>$product</td>";
-    $html .= "<td>$prijs</td>";
-    $html .= "</tr>";
-  }
-  $html .= "</table>";
-  return $html;
-}
-
+// invoer controle
+$soort = '';
 if (isset($_GET['soort'])) {
-  $htmlMenu = genereerProducten($menu[$soort]);
-} else {
-  $htmlMenu = genereerMenukaart($menu);
+  $soort = $_GET['soort'];
+  if (!isset($menu[$soort])) {
+    $soort = '';
+  }
 }
-
+function genereerMenukaart($menu, $soort = '') {
+  $html = "";
+  if ($soort) {
+    $html .= "<h2>$soort</h2>";
+    $html .= genereerProducten($menu[$soort]);
+  } else {
+    foreach ($menu as $categorie => $producten) {
+      $html .= "<h2>$categorie</h2>";
+      $html .= genereerProducten($producten);
+    }
+  }
+  return $html;
+}
+function genereerProducten($producten) {
+  $html = "<table>\n";
+  foreach ($producten as $product => $prijs) {
+    $html .= "<tr>\n";
+    $html .= "<td>$product</td>\n";
+    $html .= "<td>$prijs</td>\n";
+    $html .= "</tr>\n";
+  }
+  $html .= "</table>\n";
+  return $html;
+}
+$htmlMenu = genereerMenukaart($menu, $soort);
 ?>
 <!DOCTYPE html>
 <html lang="nl">
-
-<head>
-  <meta charset="UTF-8" />
-  <title>Restaurantmenu</title>
-  <style>
-    td:first-child {
-      width: 8em;
-    }
-
-    td:nth-child(2) {
-      font-style: italic;
-      text-align: right;
-      width: 4em;
-    }
-  </style>
-</head>
-
-<body>
-  <h1>Menu</h1>
-
-  <?= $htmlMenu ?>
-</body>
-
+  <head>
+    <meta charset="UTF-8" />
+    <title>Restaurantmenu</title>
+    <style>
+      td:first-child {
+        width: 8em;
+      }
+      td:nth-child(2) {
+        font-style: italic;
+        text-align: right;
+        width: 4em;
+      }
+    </style>
+  </head>
+  <body>
+    <form action="#" method="GET">
+      <select name="soort">
+        <option value="">Alles</option>
+        <option value="Eten">Lekker eten</option>
+        <option value="Drinken">Zuip wat</option>
+        <option value="Toetjes">Nagerecht</option>
+      </select>
+      <button type="submit">Verstuur</button>
+    </form>
+    <h1>Menu</h1>
+    
+    <?= $htmlMenu ?>
+  </body>
 </html>
--- applicatie/muziekstukken.php
+++ applicatie/muziekstukken.php
@@ -1,34 +1,58 @@
 <?php
+require_once "db_connectie.php";
+// ==== DATA LAAG ====
+// (deze functie hoort in apart bestand in de data-laag)
+function getMuziekstukken()
+{
+    // maak verbinding
+    $db = maakVerbinding();
 
-include 'db_connectie.php'
+    // stel sql samen
+    //$sql = "select stuknr, titel, genrenaam, niveaucode from stuk";
+    $sql = "select stuknr, titel, genrenaam, n.omschrijving
+  from stuk s left outer join niveau n on s.niveaucode = n.niveaucode";
 
- function haalAlleFilmsOp() {
-    $data = $verbinding->query("SELECT stuknr, titel, genrenaam, n.omschrijving FROM stuk s LEFT OUTER JOIN niveau n ON s.niveaucode = n.niveaucode");
+    // voer query uit
+    $data = $db->query($sql);
+
     return $data;
-};
+}
+// ==== VIEW LAAG ====
+// (deze functie hoort in apart bestand in de view-laag)
+function maakMuziekstukkenOverzicht($data)
+{
+    // verwerk data
+    $html = "<table>";
+    foreach ($data as $rij) {
+        $stuknr = $rij['stuknr'];
+        $titel = $rij['titel'];
+        $genrenaam = $rij['genrenaam'];
+        $omschrijving = $rij['omschrijving'];
 
-function filmsNaarHTML($films){
-    $html = '';
-    foreach($films as $film){
-
+        $html .= "<tr>";
+        $html .= "<td>$stuknr</td>";
+        $html .= "<td>$titel</td>";
+        $html .= "<td>$genrenaam</td>";
+        $html .= "<td>$omschrijving</td>";
+        $html .= "</tr>";
     }
+    $html .= "</table>";
     return $html;
 }
-
-    ?>
-
+// === code voor dit bestand ===
+$data = getMuziekstukken();
+$html = maakMuziekstukkenOverzicht($data);
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alle films</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
-
 <body>
-    <h1>Dit is een overzicht van alle films</h1>
+  <h1>Muziekstukken</h1>  
+  <?= $html ?>
 </body>
-
 </html>
